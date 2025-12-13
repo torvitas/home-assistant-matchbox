1c58aadcc0e5298c8a6e642c26e1071290b1b6c2
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2025-12-13 22:42:28 +0100

    feat: Move data folder, add arguments option

--- matchbox/data/run.sh
+++ matchbox/data/run.sh
@@ -13,30 +13,30 @@ FQDN=$(bashio::config "fqdn")
 
 cd /etc/matchbox
 if [[ "$CA_CRT" != "null" && "$CLIENT_CRT" != "null" && "$CLIENT_KEY" != "null" && "$SERVER_CRT" != "null" && "$SERVER_KEY" != "null" ]]; then
-    echo "${CA_CRT}" > ca.crt
-    echo "${CLIENT_CRT}" > client.crt
-    echo "${CLIENT_KEY}" > client.key
-    echo "${SERVER_CRT}" > server.crt
-    echo "${SERVER_KEY}" > server.key
-    chmod 644 /etc/matchbox/*.crt
-    chmod 600 /etc/matchbox/*.key
+  echo "${CA_CRT}" >ca.crt
+  echo "${CLIENT_CRT}" >client.crt
+  echo "${CLIENT_KEY}" >client.key
+  echo "${SERVER_CRT}" >server.crt
+  echo "${SERVER_KEY}" >server.key
+  chmod 644 /etc/matchbox/*.crt
+  chmod 600 /etc/matchbox/*.key
 elif [[ ! -s "ca.crt" || ! -s "client.crt" || ! -s "client.key" || ! -s "server.crt" || ! -s "server.key" ]]; then
-    if [[ "$FQDN" != "null" ]]; then
-        cd /scripts/tls
-        export SAN="DNS.1:${FQDN}"
-        ./cert-gen
-        cp ca.crt client.crt client.key server.crt server.key /etc/matchbox/
-        bashio::log.green "ca.crt: \n $(<ca.crt)"
-        bashio::log.green "client.crt: \n $(<client.crt)"
-        bashio::log.green "client.key: \n $(<client.key)"
-        cd /etc/matchbox
-    else
-        bashio::log.fatal "Configuration invalid, either specify fqdn or a full set of certificates..."
-        exit 1
-    fi
+  if [[ "$FQDN" != "null" ]]; then
+    cd /scripts/tls
+    export SAN="DNS.1:${FQDN}"
+    ./cert-gen
+    cp ca.crt client.crt client.key server.crt server.key /etc/matchbox/
+    bashio::log.green "ca.crt: \n $(<ca.crt)"
+    bashio::log.green "client.crt: \n $(<client.crt)"
+    bashio::log.green "client.key: \n $(<client.key)"
+    cd /etc/matchbox
+  else
+    bashio::log.fatal "Configuration invalid, either specify fqdn or a full set of certificates..."
+    exit 1
+  fi
 fi
 cd ~
 
 bashio::log.info "Starting matchbox..."
 
-exec /matchbox
+exec /matchbox $(bashio::config "arguments")
